7738c890437c0c14b62e6f2e018eaa6c4237f940
Yuna Lee <yunalee22@gmail.com>
2017-04-25 19:04:26 -0700

    Basic navigation functionality

---
+++ FBSearch/AboutViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutViewController.swift
+//  FBSearch
+//
+//  Created by Yuna Lee on 4/25/17.
+//  Copyright © 2017 USC. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up slide out menu
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ FBSearch/MenuViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MenuViewController.swift
+//  FBSearch
+//
+//  Created by Yuna Lee on 4/25/17.
+//  Copyright © 2017 USC. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.row) {
+        case 1:
+            self.performSegue(withIdentifier: "show_home", sender: self)
+            break;
+        case 2:
+            self.performSegue(withIdentifier: "show_favorites", sender: self)
+            break;
+        case 3:
+            self.performSegue(withIdentifier: "show_about", sender: self)
+            break;
+        default:
+            break;
+        }
+    }*/
+    
+}
